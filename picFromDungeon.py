e18bbe19ad25b74b0e64c223d5035b070ffb35aa
lucieneckert <lucien.eckert@gmail.com>
2022-09-22 22:01:16 -0400

    Moved from filepaths for each tile to cropped regions of one atlas

--- picFromDungeon.py
+++ picFromDungeon.py
@@ -4,40 +4,24 @@ from generateDungeon import Dungeon
 from PIL import Image
 import numpy as np
 
-groundTilePath = "tiles/test-ground.png"
-wallTilePath = "tiles/test-innerwall.png"
-LwallTilePath = "tiles/test-Lwall.png"
-RwallTilePath = "tiles/test-Rwall.png"
-BLwallTilePath = "tiles/test-BLwall.png"
-BRwallTilePath = "tiles/test-BRwall.png"
-TLwallTilePath = "tiles/test-TLwall.png"
-TRwallTilePath = "tiles/test-TRwall.png"
-TwallTilePath = "tiles/test-Twall.png"
-BwallTilePath = "tiles/test-Bwall.png"
-TLcornerWallTilePath = "tiles/test-TLcornerWall.png"
-TRcornerWallTilePath = "tiles/test-TRcornerWall.png"
-BLcornerWallTilePath = "tiles/test-BLcornerWall.png"
-BRcornerWallTilePath = "tiles/test-BRcornerWall.png"
-
 testTileset = "tiles/test.png"
 
 
-tile_path_from_value = {
-    0: wallTilePath,
-    1: groundTilePath,
-    2: TwallTilePath,
-    3: RwallTilePath,
-    4: BwallTilePath,
-    5: LwallTilePath,
-    6: TLwallTilePath,
-    7: TRwallTilePath,
-    8: BLwallTilePath,
-    9: BRwallTilePath,
-    10: TLcornerWallTilePath,
-    11: TRcornerWallTilePath,
-    12: BLcornerWallTilePath,
-    13: BRcornerWallTilePath,
-    14: groundTilePath,
+tile_crop_from_value : dict[int, tuple[int, int, int, int]] = {
+    0: (5 * 32, 0, 6 * 32, 32),
+    2: (384, 0, 416, 32),
+    3: (7 * 32, 0, 8 * 32, 32),
+    4: (128, 0, 160, 32),
+    5: (6 * 32, 0, 7 * 32, 32),
+    6: (9 * 32, 0, 10 * 32, 32),
+    7: (11 * 32, 0, 12 * 32, 32),
+    8: (32, 0, 64, 32),
+    9: (3 * 32, 0, 4 * 32, 32),
+    10: (8 * 32, 0, 9 * 32, 32),
+    11: (10 * 32, 0, 11 * 32, 32),
+    12: (0, 0, 32, 32),
+    13: (2 * 32, 0, 3 * 32, 32),
+    14: (0, 32, 32, 64),
 }
 
 def get_random_ground(tileset):
@@ -54,14 +38,16 @@ def image_from_dungeon(dungeon, tileset):
     # Paste floors on the ground
     for x, y in np.ndindex(dungeon.shape):
         image.paste(get_random_ground(tileset), (32 * x, 32 * y))
-    # TODO: Iterate over the tiles in the dungeon, paste images
+    # Iterate over the tiles in the dungeon, paste images
     for x, y in np.ndindex(dungeon.shape):
         if dungeon[x, y] != 1:
-            tile = Image.open(tile_path_from_value[dungeon[x, y]])
+            tile = tileset.crop(
+                tile_crop_from_value[dungeon[x, y]]
+            )
             image.paste(tile, (32 * x, 32 * y))
     # Show the generated image
     image.show()
 
 # Testing:
-dungeon = Dungeon([15, 8])
+dungeon = Dungeon([15, 15])
 image_from_dungeon(dungeon, testTileset)
